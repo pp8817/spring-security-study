c8eb92c2c68c03b481f80661c855a16601bc954d
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-04-29 17:52:09 +0900

    Spring Security 6.x 버전으로 개정되면서 변화된 ContextHolder에서 context 관련 작업을 하는 과정을 공부함 - 이제 사이에 HolderStrategt를 호출해야한다. 이렇게 함으로써 전략 패턴을 자동 주입하도록 한다

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,16 +1,24 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    @Autowired
+    SecurityContextService securityContextService;
+
     @GetMapping("/")
     public String index() {
+        securityContextService.securityContext();
+
         return "index";
     }
 
