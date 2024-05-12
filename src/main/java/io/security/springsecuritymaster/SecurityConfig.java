e5072eeba82092b388e7116567467d64992009e7
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-12 16:23:46 +0900

    요청 기반 권한 부여 - HttpSecurity.authorizeHttpRequest() 공부

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,36 +2,41 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-//        CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
-        XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
-//
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/csrf", "/csrfToken").permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/user").hasAuthority("ROLE_USER") // "/user" 엔드 포인트에 대해 "USER" 권한을 요구한다.
+                        .requestMatchers("/myPage/**").hasRole("USER") // "/mypage" 및 하위 디렉토리에 대해 "USER" 권한을 요구한다.
+                        .requestMatchers(HttpMethod.POST).hasAuthority("ROLE_WRITE") // POST 메소드를 사용하는 모든 요청에 대해 "write" 권한을 요구한다.
+                        .requestMatchers(new AntPathRequestMatcher("/manager/**"))
+                        .hasAuthority("ROLE_MANAGER") // "/manager" 및 하위 디렉토리에 대해 "manage" 권한을 요구한다.
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/admin/payment")).hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MANAGER") // "/admin" 및 하위 디렉토리에 대해 "admin" 또는 "manager" 권한을 요구한다.
+                        .requestMatchers(new RegexRequestMatcher("/resource/[A-Za-z0-9]+", null)).hasAuthority("ROLE_MANAGER")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-//                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-//                )
-
-        ;
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
@@ -39,6 +44,10 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+
+        UserDetails manager = User.withUsername("manager").password("{noop}1111").roles("MANAGER").build();
+
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN", "WRITE").build();
         return new InMemoryUserDetailsManager(user);
     }
 
